bc047090b434c0b044ef6de886c039f219550b7d
howlingunit <tyler_pitt@outlook.com>
2022-08-17 00:34:04 +0100

    testimonials db complete

--- static/lib/index.js
+++ static/lib/index.js
@@ -11,23 +11,8 @@ async function aboutMeHeight() {
 }
 
 async function insertTestimonials() {
-  const testimonials =[];
-
-  let testimonial_1 = await fetch('/testimonial');
-  testimonial_1 = await testimonial_1.json();
-  testimonial_1 = testimonial_1[0];
-  testimonials.push(testimonial_1)
-  
-  let testimonial_2 = await fetch('/testimonial');
-  testimonial_2 = await testimonial_2.json();
-  testimonial_2 = testimonial_2[0];
-
-  while (testimonial_1.name === testimonial_2.name) {
-    let testimonial_2 = await fetch('/testimonial');
-    testimonial_2 = await testimonial_2.json();
-    testimonial_2 = testimonial_2[0];
-  }
-  testimonials.push(testimonial_2)
+  let testimonials = await fetch('/testimonial?amt=2');
+  testimonials = await testimonials.json();
 
   for (let i = 0; i<2; i++) {
 
